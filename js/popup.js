c568aeaf923cc3cf69c341c2e21e6f18402c4c46
mrqy <2012726235@qq.com>
2024-06-08 23:30:17 +0800

    Site updated: 2024-06-08 23:30:10

--- js/popup.js
+++ js/popup.js
@@ -43,22 +43,9 @@ document.addEventListener("copy", function () {
 })
 
 
-// 分享成功提醒
-document.addEventListener("share", function () {
-    debounce(function () {
-        new Vue({
-            data: function () {
-                this.$notify({
-                    title: "分享成功！",
-                    message: "分享成功，给你一个大大的赞！",
-                    position: 'top-left',
-                    offset: 50,
-                    showClose: true,
-                    type: "success",
-                    duration: 5000
-                });
-            }
-        })
-    }, 300);
-})
-
+// 分享本页
+function share() {
+    let url = window.location.origin + window.location.pathname
+    new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
+    btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+}
